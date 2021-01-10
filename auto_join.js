c5ce67729fbc283268c2099bf57db14f1982fd36
SamYung-OfficialAccount <65942509+SamYung-OfficialAccount@users.noreply.github.com>
2021-01-10 09:16:17 +0800

    Update auto_join.js

--- auto_join.js
+++ auto_join.js
@@ -1,56 +1,113 @@
-
-    // MeetHax was originally called XYZBot.
-    /*
+// MeetHax was originally called XYZBot.
+/*
     ***************************************
     Changelog:
 	- Userscript converted into extension.
     ***************************************
     */
-    'use strict';
-    window.onload = function(){
-        window.setInterval(function(){
-        if((document.body.innerHTML.includes('<div class="jtEd4b">You can\'t create a meeting yourself.') || document.body.innerHTML.includes('<div jsname="r4nke" class="CRFCdf">This meeting hasn\'t started yet</div>')) || document.body.innerHTML.includes('<div jsname="r4nke" class="CRFCdf">Someone has removed you from the meeting</div>')){
-            //Hitman mode
-            location.reload(); // Reload if the Google Meet hasn't started. We want to reload it until it starts.
-        }
-        else if (document.body.innerHTML.includes("<div class=\"Xzzw9d\">Cast this meeting</div>")){
-            // AutoJoin mode
-            try{document.querySelector('[data-tooltip="Turn off microphone (ctrl + d)"]').click();}catch(e){} // Turn off your microphone. Nobody wants to hear your cat.
-            try{document.querySelector('[data-tooltip="Turn off camera (ctrl + e)"]').click();}catch(e){} // Turn off your camera. Nobody wants to see your clothes wardrobe.
-            try{document.querySelector('[jsname="Qx7uuf"]').click();}catch(e){} // Join now!
-            try{document.querySelector('[jsname="Qx7uuf"]').click();}catch(e){} // Join now!
-			const buttons = [
-							{
-							label: 'Microphone',
-							storageName: 'disableMic',
-							buttonKey: 'Turn off microphone (ctrl + d)',
-							direction: 'right',
-							},
+'use strict'
+let autoJoin = () => {
+	try {
+		document.querySelector('[data-tooltip="Turn off microphone (ctrl + d)"]').click();
+	} catch (e) {} // Turn off your microphone. Nobody wants to hear your cat.
+	try {
+		document.querySelector('[data-tooltip="Turn off camera (ctrl + e)"]').click();
+	} catch (e) {} // Turn off your camera. Nobody wants to see your clothes wardrobe.
+	try {
+		document.querySelector('[jsname="Qx7uuf"]').click();
+	} catch (e) {} // Join now!
+	try {
+		document.querySelector('[jsname="Qx7uuf"]').click();
+	} catch (e) {} // Join now!
+	const buttons = [{
+			label: 'Microphone',
+			storageName: 'disableMic',
+			buttonKey: 'Turn off microphone (ctrl + d)',
+			direction: 'right',
+		},
 		{
-      label: 'Camera',
-      storageName: 'disableCam',
-      buttonKey: 'Turn off camera (ctrl + e)',
-      direction: 'left',
-    },
-  ];
-  
-  buttons.forEach(({label, storageName, buttonKey, direction}) => {
-    
-    /** @type {HTMLDivElement} */
-    const button = document.querySelector(`div[role="button"][data-tooltip$=" + ${buttonKey})" i][data-is-muted]`);
-    if (!button) return;
-    
-    /** @type {boolean} */
-    
-    /** @return {void} */
-    const disable = () => {
-      if (button.dataset.isMuted === 'false')
-        button.click();
-    };
-	
-	disable();
-  });
-    
-        }
-},1000);
-    };
+			label: 'Camera',
+			storageName: 'disableCam',
+			buttonKey: 'Turn off camera (ctrl + e)',
+			direction: 'left',
+		},
+	];
+
+	buttons.forEach(({
+		label,
+		storageName,
+		buttonKey,
+		direction
+	}) => {
+
+		/** @type {HTMLDivElement} */
+		const button = document.querySelector(`div[role="button"][data-tooltip$=" + ${buttonKey})" i][data-is-muted]`);
+		if (!button) return;
+
+		/** @type {boolean} */
+
+		/** @return {void} */
+		const disable = () => {
+			if (button.dataset.isMuted === 'false')
+				button.click();
+		};
+
+		disable();
+	});
+};
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.clear) {
+		delete localStorage.flagUpdate;
+	}
+});
+window.onload = function () {
+	window.setInterval(function () {
+		if ((document.body.innerHTML.includes('<div class="jtEd4b">You can\'t create a meeting yourself.') || document.body.innerHTML.includes('<div jsname="r4nke" class="CRFCdf">This meeting hasn\'t started yet</div>')) || document.body.innerHTML.includes('<div jsname="r4nke" class="CRFCdf">Someone has removed you from the meeting</div>')) {
+			//Hitman mode
+			if (localStorage["flagUpdate"]) {
+				try {
+					chrome.runtime.sendMessage({
+						joined: (document.body.innerHTML.includes('<div jsname="r4nke" class="CRFCdf">This meeting hasn\'t started yet</div>')),
+						currentLocation: location.href
+					});
+				} catch (e) {}
+				location.reload();
+			
+			chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+				if (request.verified && request.sender == "detect_time") {
+					localStorage.flagUpdate = true;
+					sendResponse({
+						joined: (document.body.innerHTML.includes('<div jsname="r4nke" class="CRFCdf">This meeting hasn\'t started yet</div>')),
+						currentLocation: location.href
+					});
+					location.reload(); // Reload if the Google Meet hasn't started. We want to reload it until it starts.
+				}
+			});
+			location.reload(); // Reload if the Google Meet hasn't started. We want to reload it until it starts.
+		}} 
+		else if (document.body.innerHTML.includes("<div class=\"Xzzw9d\">Cast this meeting</div>")) {
+			if (localStorage["flagUpdate"]) {
+				localStorage["flagUpdate"] = false;
+				// AutoJoin mode
+				try {
+					chrome.runtime.sendMessage({
+						joined: true,
+						currentLocation: location.href
+					});
+				} catch (e) {}
+				autoJoin(); //Automatic Join
+			}
+			chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+				if (request.verified && request.sender == "detect_time") {
+					localStorage.flagUpdate = true;
+					sendResponse({
+						joined: true,
+						currentLocation: location.href
+					});
+					autoJoin(); // Automatic Join
+				}
+			});
+			autoJoin();
+		}
+	}, 1000);
+};
